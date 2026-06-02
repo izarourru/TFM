45263c29de3589c5fd3bb419fb91134cb25229d6
izaro <izarourru@gmail.com>
2026-06-02 09:29:36 +0200

    add harmonie data

--- load_data.R
+++ load_data.R
@@ -1,6 +1,6 @@
 library(loadeR)
 
-load_data <- function(year, lonLim, latLim, what, dataset) {
+load_data <- function(year, lonLim, latLim, what, dataset, mask = TRUE) {
   if (dataset == "ERA5") {
     if (what == "Wind") {
       var <- "sfcwind"
@@ -17,11 +17,19 @@ load_data <- function(year, lonLim, latLim, what, dataset) {
       var <- what
       f <- paste0("/lustre/gmeteo/WORK/urrutxuai/data/data_derived/fwi/", what, "_CIDE.nc")
     }
+  } else if (dataset == "HARMONIE") {
+    if (what == "Wind") {
+      var <- "sfcWind"
+      f <- "/lustre/gmeteo/WORK/urrutxuai/data/ncml/HARMONIE_sfcWind_0.1grid.ncml"
+    } else {
+      var <- what
+      f <- paste0("/lustre/gmeteo/WORK/urrutxuai/data/data_derived/fwi/", what, "_HARMONIE.nc")
+    }
   }
   
   dat <- loadGridData(f, var = var, latLim = latLim, lonLim = lonLim, years = year)
   
-  if (dataset == "ERA5" && identical(latLim, c(36, 44)) && identical(lonLim, c(-9.5, 4.3))) {
+  if (mask == TRUE && dataset == "ERA5" && identical(latLim, c(36, 44)) && identical(lonLim, c(-9.5, 4.3))) {
     load("/lustre/gmeteo/WORK/urrutxuai/R/mask_2d.RData")
     dat$Data <- sweep(dat$Data, c(2, 3), mask_2d, FUN = "*")
   }
